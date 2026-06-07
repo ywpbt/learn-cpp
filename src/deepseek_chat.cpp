1abef09ab664528201c6d79161a52e27c96bd712
ywpbt <ywp.yywwpp@gmail.com>
2026-06-08 01:48:49 +0800

    update deepseek_chat implementation

--- src/deepseek_chat.cpp
+++ src/deepseek_chat.cpp
@@ -10,6 +10,36 @@ using n_json = nlohmann::json;
 
 char print_char(const char*, size_t);
 
+std::string read_mutiline_input()
+{
+    printf("\n*YOU* (输入 exit 单独一行结束) \n");
+    printf(">");
+
+    std::string inputs;
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        if (line == "exit") {
+            break;
+        }
+        inputs += line + "\n";
+
+        if (!std::cin.eof()) {
+            printf(">");
+        }
+    }
+
+    if (!inputs.empty()) {
+        inputs.pop_back();
+    }
+
+    if (std::cin.eof()) {
+        std::cin.clear();
+    }
+
+    return inputs;
+}
+
 void live_chat()
 {
     const char* apikey = std::getenv("APIKEY");
@@ -35,90 +65,120 @@ void live_chat()
         {"reasoning_effort", "max"},
         {"messages", {{{"role", "system"}, {"content", "You are a helpful assistant."}}}},
     };
-    payload["messages"].push_back(
-        {{"role", "user"},
-         {"content", "我想要知道现在的时间点2026年6月为止gcc各版本对应的C++标准能让我在工作中能方便的选择适合项目的C++标准."}});
-    std::cout << payload.dump() << "\n\n";
+    // payload["messages"].push_back(
+    //     {{"role", "user"},
+    //      {"content",
+    //      "我想要知道现在的时间点2026年6月为止gcc各版本对应的C++标准能让我在工作中能方便的选择适合项目的C++标准."}});
+    // std::cout << payload.dump() << "\n\n";
 
-    bool has_shown_hint = true;
-    bool is_integrity = true;
+    while (true) {
+        std::string inputs = read_mutiline_input();
+        if (inputs.empty()) {
+            // print_char(inputs.c_str(), inputs.size());
+            break;
+        }
 
-    auto stream = httplib::stream::Post(cli, "/chat/completions", payload.dump(), "application/json");
-    if (stream && stream.status() == 200) {
-        std::string chunked;
+        payload["messages"].push_back({
+            {"role", "user"},
+            {"content", inputs},
+        });
+        std::cout << payload.dump() << "\n\n";
 
-        while (stream.next()) {
-            chunked.append(stream.data(), stream.size());
-            if (chunked.back() != '\n' && chunked.back() != '\0') {
-                is_integrity = false;
-                continue;
-            }
-            if (!is_integrity) {
-                is_integrity = true;
-            }
+        // if (!inputs.empty()) {
+        //     std::cout << payload << "\n\n";
+        //     break;
+        // }
 
-            std::istringstream iss(chunked);
-            std::string line;
+        std::string assistant_reply;
+        bool has_shown_hint = true;
+        bool is_integrity = true;
 
-            while (std::getline(iss, line)) {
-                if (line.empty()) {
+        auto stream = httplib::stream::Post(cli, "/chat/completions", payload.dump(), "application/json");
+        if (stream && stream.status() == 200) {
+            std::string chunked;
+
+            while (stream.next()) {
+                chunked.append(stream.data(), stream.size());
+                if (chunked.back() != '\n' && chunked.back() != '\0') {
+                    is_integrity = false;
                     continue;
                 }
+                if (!is_integrity) {
+                    is_integrity = true;
+                }
 
-                if (line.starts_with("data:")) {
+                std::istringstream iss(chunked);
+                std::string line;
 
-                    std::string json_str = line.substr(6);
-                    if (json_str == "[DONE]") {
-                        break;
-                    }
-                    n_json json;
-                    try {
-                        json = n_json::parse(json_str);
-                    }
-                    catch (const std::exception& e) {
-                        std::cerr << "\nJSON解析错误:" << e.what() << '\n';
-                        std::cerr << "\n" << "json_str:" << json_str << "\n\n";
-                        std::cerr << "\n" << "line:" << line << "\n\n";
-                        std::cerr << "\n" << "chunked:" << chunked << "\n\n";
-                        std::cerr << "\n" << "stream data:" << stream.data() << "\n" << "stream size:" << stream.size() << "\n\n";
-                        return;
+                while (std::getline(iss, line)) {
+                    if (line.empty()) {
+                        continue;
                     }
 
-                    try {
+                    if (line.starts_with("data:")) {
 
-                        auto reasoning = json["choices"][0]["delta"]["reasoning_content"];
-                        if (!reasoning.empty() && !reasoning.is_null()) {
-                            if (has_shown_hint) {
-                                std::cout << "[思考]: " << std::flush;
-                                has_shown_hint = false;
-                            }
-                            std::cout << reasoning.get<std::string>() << std::flush;
+                        std::string json_str = line.substr(6);
+                        if (json_str == "[DONE]") {
+                            break;
+                        }
+                        n_json json;
+                        try {
+                            json = n_json::parse(json_str);
+                        }
+                        catch (const std::exception& e) {
+                            std::cerr << "\nJSON解析错误:" << e.what() << '\n';
+                            std::cerr << "\n" << "json_str:" << json_str << "\n\n";
+                            std::cerr << "\n" << "line:" << line << "\n\n";
+                            std::cerr << "\n" << "chunked:" << chunked << "\n\n";
+                            std::cerr << "\n"
+                                      << "stream data:" << stream.data() << "\n"
+                                      << "stream size:" << stream.size() << "\n\n";
+                            return;
                         }
 
-                        auto answer = json["choices"][0]["delta"]["content"];
-                        if (!answer.empty() && !answer.is_null()) {
-                            if (!has_shown_hint) {
-                                std::cout << "\n\n";
-                                std::cout << "[回复]: " << std::flush;
-                                has_shown_hint = true;
+                        try {
+
+                            auto reasoning = json["choices"][0]["delta"]["reasoning_content"];
+                            if (!reasoning.empty() && !reasoning.is_null()) {
+                                if (has_shown_hint) {
+                                    std::cout << "[思考]: " << std::flush;
+                                    has_shown_hint = false;
+                                }
+                                std::cout << reasoning.get<std::string>() << std::flush;
                             }
-                            std::cout << answer.get<std::string>() << std::flush;
+
+                            auto answer = json["choices"][0]["delta"]["content"];
+                            if (!answer.empty() && !answer.is_null()) {
+                                if (!has_shown_hint) {
+                                    std::cout << "\n\n";
+                                    std::cout << "[回复]: " << std::flush;
+                                    has_shown_hint = true;
+                                }
+
+                                std::cout << answer.get<std::string>() << std::flush;
+                                assistant_reply.append(answer.get<std::string>());
+                            }
                         }
-                    }
-                    catch (std::exception& e) {
-                        std::cerr << "JSON读取错误:" << e.what() << "\n";
-                        return;
+                        catch (std::exception& e) {
+                            std::cerr << "JSON读取错误:" << e.what() << "\n";
+                            return;
+                        }
                     }
                 }
-            }
 
-            if (is_integrity) {
-                chunked.clear();
+                if (is_integrity) {
+                    chunked.clear();
+                }
             }
         }
-    }
-    else {
-        std::cerr << stream.status() << ":" << stream.error() << "\n";
+        else {
+            std::cerr << stream.status() << ":" << stream.error() << "\n";
+        }
+        payload["messages"].push_back({
+            {"role", "assistant"},
+            {"content", assistant_reply},
+        });
+        assistant_reply.clear();
     }
     std::cout << "\n";
 }
@@ -130,12 +190,12 @@ char print_char(const char* str, size_t len)
     const char* start = data;
     const char* end = start + len;
 
-    printf("%.*s", (int)len, data);
-    printf("======\n");
+    printf("\n%.*s", (int)len, data);
+    printf("\n======\n");
 
     char returned = -1;
 
-    for (const char* p = data; p < end; p++) {
+    for (const char* p = data; p <= end; p++) {
         switch (*p) {
         case '\r':
             printf("\\r\n");
