b71e3849b017f0f987831d7bddb41ad4d7135ca6
ywpbt <ywp.yywwpp@gmail.com>
2026-06-08 11:09:04 +0800

    refactor: clean up CMakeLists formatting, add deepseek executable, restore main.cpp to use math_utils and http_utils

---
+++ src/deepseek.cpp
@@ -0,0 +1,10 @@
+#include <spdlog/spdlog.h>
+#include "deepseek_chat.h"
+
+int main(int argc, char** argv)
+{
+    spdlog::info("deepseek chat:\n");
+    live_chat();
+
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
-// #include "http_utils.h"
-// #include "math_utils.h"
-#include "deepseek_chat.h"
+#include "http_utils.h"
+#include "math_utils.h"
 
 int main(int argc, char* argv[])
 {
     std::cout << "argc:" << argc << ",argv:" << argv[0] << "\n";
     std::cout << "current c++ version:" << __cplusplus << "\n";
 
-    // spdlog::info("Calling add function from math_utils:");
-    // int sum1 = add(200, 199);
-    // spdlog::info("The sum of 200 and 199 is {}", sum1);
+    spdlog::info("Calling add function from math_utils:");
+    int sum1 = add(200, 199);
+    spdlog::info("The sum of 200 and 199 is {}", sum1);
 
-    // spdlog::info("Getting IP address:");
-    // get_ip_address();
+    spdlog::info("Getting IP address:");
+    get_ip_address();
 
-    // spdlog::info("Getting request headers:");
-    // get_req_headers();
-
-    spdlog::info("nlohmann json example:\n");
-    live_chat();
+    spdlog::info("Getting request headers:");
+    get_req_headers();
 
     return 0;
 }
